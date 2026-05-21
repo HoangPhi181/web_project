5b938016377dd9616cbd78869b860341cd7f8d15
HoangPhi181 <n23dccn181@student.ptithcm.edu.vn>
2026-05-22 01:24:52 +0700

    fix lỗi tên file khi render

--- backend/mediators/CentralMediator.js
+++ backend/mediators/CentralMediator.js
@@ -18,9 +18,9 @@
 //   await Mediator.Wallet.deposit(userId, amount)
 //   await Mediator.Admin.blockUser(adminId, userId)
 
-const { Auth }   = require("./authMediator");
-const { Trade }  = require("./tradeMediator");
-const { Wallet } = require("./walletMediator");
-const { Admin }  = require("./adminMediator");
+const { Auth }   = require("./AuthMediator");
+const { Trade }  = require("./TradeMediator");
+const { Wallet } = require("./WalletMediator");
+const { Admin }  = require("./AdminMediator");
 
 module.exports = { Auth, Trade, Wallet, Admin };
